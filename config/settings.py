9accfe908884a43e573c4d67c26dd05c94825e8d
denis <den.listapad@mail.ru>
2024-12-26 20:55:05 +0300

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -99,9 +99,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME", "tasktracker"),
-        'HOST': os.environ.get("DB_HOST", "postgres"),
-        'USER': os.environ.get("DB_USER", "postgres"),
-        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'USER': os.environ.get("DB_USER", "denis"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "1234"),
         'PORT': os.environ.get("DB_PORT", 5432),
         'TEST': {
             'NAME': os.environ.get("DB_TEST_NAME", "task_tracker_test"),
